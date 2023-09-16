e13fd0a0ee1336f58054f8f495516a708cbb3514
Roman_Strokov <ready3k@gmail.com>
2023-09-16 14:07:13 +0300

    update 16/09 HW7 added

--- pages/demoqa.py
+++ pages/demoqa.py
@@ -1,18 +1,12 @@
-from selenium.common.exceptions import NoSuchElementException
 from pages.base_page import BasePage
+from components.components import WebElement
 
 
 class DemoQA(BasePage):
 
-    def exist_icon(self):
-        try:
-            self.find_element('#app > header > a')
-        except NoSuchElementException:
-            return False
-        return True
-
-    def click_on_the_icon(self):
-        self.find_element('#app > header > a').click()
-
-    def equal_url(self):
-        return self.get_url() == 'https://demoqa.com/'
+    def __init__(self, driver):
+        self.base_url = 'https://demoqa.com/'
+        super().__init__(driver, self.base_url)
+        self.icon = WebElement(driver, '#app > header > a')
+        self.btn_elements = WebElement(driver, '#app > div > div > div.home-body > div > div:nth-child(1)')
+        self.footer_text = WebElement(driver, '#app > footer >span')
